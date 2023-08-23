21c6dadf6700fc54df005d7547ff3580cd9b56ff
d parvithra <dparvithra144@gmail.com>
2023-08-23 07:32:54 +0530

    second Commit

---
+++ accounts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+def home(request):
+    return render(request,'index.html')
+
+def register(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+
+        user=User.objects.create_user(username=username,email=email,password=password)
+        user.save()
+
+        return redirect('home')
+
+    return render(request,'register.html')
